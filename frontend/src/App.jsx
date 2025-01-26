2c540885b092b459693431b2c2a942de491956ad
Het-Dhorajiya-0305 <hetdhorajiya03@gmail.com>
2025-01-27 00:21:45 +0530

    basic design ready

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import {BrowserRouter as Router,Route} from 'react-router-dom';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
 import Home from './component/Home';
 import Chat from './component/Chat';
@@ -11,8 +9,11 @@ function App() {
 
   return (
     <Router>
-      <Route path='/' element={<Home></Home>}></Route>
-      <Route path='/chat' element={<Chat></Chat>}></Route>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/chat' element={<Chat></Chat>}></Route>
+      </Routes>
+
     </Router>
   )
 }
--- frontend/src/component/Chat.jsx
+++ frontend/src/component/Chat.jsx
@@ -1,11 +1,59 @@
 import React from 'react'
+import { IoSendSharp } from "react-icons/io5";
+
+import './chat.css'
 
 function Chat() {
-  return (
-    <div>
-      Chat
-    </div>
-  )
+    return (
+        <div className="container">
+            <div className="card chat-app">
+                <div id="plist" className="people-list">
+                    <div className="input-group">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text"><i className="fa fa-search"></i></span>
+                        </div>
+                    </div>
+                    <ul className="list-unstyled chat-list mt-2 mb-0">
+                        <li className="clearfix">
+                            <div className="about">
+                                <div className="name">Vincent Porter</div>
+                                <div className="status"> <i className="fa fa-circle offline"></i> left 7 mins ago </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div className="chat">
+                    <div className="chat-header clearfix">
+                        <div className="row">
+                            <div className="col-lg-6">
+                                <div className="chat-about">
+                                    <h6 className="m-b-0">Aiden Chavez</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="chat-history">
+                        <ul className="m-b-0">
+                            <li className="clearfix">
+                                <div className="message other-message float-right"> Hi Aiden, how are you? How is the project coming along? </div>
+                            </li>
+
+                        </ul>
+                    </div>
+                    <div className="chat-message clearfix">
+                        <div className="input-group mb-0">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text"><IoSendSharp size={35} />
+                                </span>
+                            </div>
+                            <input type='text' className='form-control' placeholder="Enter text here..."></input>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
 }
 
 export default Chat
