55b62482ee825bfbd9760cd929b60f95edf0c888
Het-Dhorajiya-0305 <hetdhorajiya03@gmail.com>
2025-01-28 01:09:52 +0530

    chat page almost done

--- frontend/src/component/Chat.jsx
+++ frontend/src/component/Chat.jsx
@@ -1,58 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoSendSharp } from "react-icons/io5";
 
 import './chat.css'
 
 function Chat() {
+    const [message,setMessage]=useState("");
     return (
-        <div className="container">
-            <div className="card chat-app">
-                <div id="plist" className="people-list">
-                    <div className="input-group">
-                        <div className="input-group-prepend">
-                            <span className="input-group-text"><i className="fa fa-search"></i></span>
-                        </div>
-                    </div>
-                    <ul className="list-unstyled chat-list mt-2 mb-0">
-                        <li className="clearfix">
-                            <div className="about">
-                                <div className="name">Vincent Porter</div>
-                                <div className="status"> <i className="fa fa-circle offline"></i> left 7 mins ago </div>
-                            </div>
-                        </li>
-                    </ul>
+        <div className="chat">
+            <div className="left">
+                <div className='people-heading'>people</div>
+                <div className="people_list">
+                    <div className="even" id='people'>het dhorajiya</div>
+                    <div className="odd" id='people'>het dhorajiya</div>
+                    <div className="even" id='people'>het dhorajiya</div>
+                    <div className="odd" id='people'>het dhorajiya</div>
+                    <div className="even" id='people'>het dhorajiya</div>
+                    <div className="odd" id='people'>het dhorajiya</div>
+                    <div className="even" id='people'>het dhorajiya</div>
+                    <div className="odd" id='people'>het dhorajiya</div>
+                    <div className="even" id='people'>het dhorajiya</div>
+                    <div className="odd" id='people'>het dhorajiya</div>
+                    <div className="even" id='people'>het dhorajiya</div>
+                    <div className="odd" id='people'>het dhorajiya</div>
+                    <div className="even" id='people'>het dhorajiya</div>
+                    <div className="odd" id='people'>het dhorajiya</div>
+                    <div className="even" id='people'>het dhorajiya</div>
                 </div>
-                <div className="chat">
-                    <div className="chat-header clearfix">
-                        <div className="row">
-                            <div className="col-lg-6">
-                                <div className="chat-about">
-                                    <h6 className="m-b-0">Aiden Chavez</h6>
-                                </div>
-                            </div>
+            </div>
+            <div className="right">
+                <div className="right-top">
+                    <div className="chat-people-name">
+                        <span>het dhorajiya</span>
+                    </div>
+                    <div className="right-middle">
+                        <div className="messages">
+                            <div id="message" className='float-right'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quo, ipsam officia quos, unde autem doloribus voluptatem doloremque libero sint earum perferendis repellat dignissimos id, quidem reprehenderit modi! Doloremque, sint?</div>
+                            <div id="message" >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat, quidem.</div>
+                            <div id="message">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat, quidem.</div>
+                            <div id="message">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat, quidem.</div>
+                            <div id="message">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat, quidem.</div>
+                            <div id="message">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat, quidem.</div>
+                            <div id="message">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat, quidem.</div>
+                            <div id="message">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat, quidem.</div> 
                         </div>
-                    </div>
-                    <div className="chat-history">
-                        <ul className="m-b-0">
-                            <li className="clearfix">
-                                <div className="message other-message float-right"> Hi Aiden, how are you? How is the project coming along? </div>
-                            </li>
-
-                        </ul>
-                    </div>
-                    <div className="chat-message clearfix">
-                        <div className="input-group mb-0">
-                            <div className="input-group-prepend">
-                                <span className="input-group-text"><IoSendSharp size={35} />
-                                </span>
-                            </div>
-                            <input type='text' className='form-control' placeholder="Enter text here..."></input>
+                        <div className="send">
+                            <button className="sendbtn"><IoSendSharp size={37} onClick={()=>setMessage("")}></IoSendSharp></button>
+                            <input type="text" name="" id="" className='input-send' value={message} onChange={(e)=>setMessage(e.target.value)}/>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
     )
 }
 
